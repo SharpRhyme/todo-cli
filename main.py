9b596b0bd64c044adeabb51a75a5f3d2982e9c92
SharpRhyme <aarizdhalla@hotmail.com>
2023-10-22 20:31:16 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Welcome to the CLI todo list")
+
+while True:
+    command = input("""
+          a - add todo
+          d - delete todo
+          v - view todo
+          c - mark todo as checked
+          q - quit
+          
+          Enter Command: """)
+    
+    if command == "a":
+        pass
+    elif command == "d":
+        pass
+    elif command == "v":
+        pass
+    elif command == "c":
+        pass
+    elif command == "q":
+        break
+    else:
+        print("\nInvalid Command")
